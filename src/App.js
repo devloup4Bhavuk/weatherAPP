9c4f9807ab1ff8ce0f5b4a2e63b8caab063d63ae
develoup4Bhavuk <devdhawan222@gmail.com>
2021-05-05 10:54:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react'
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React,{useState} from 'react'
+import weatherapi from './api/weatherApi'
+const App = () => {
+    const [city, setcity] = useState();
+    const [weather, setweather] = useState();
+    const input = (e)=>{
+        let name =e.target.name;
+        if(name === "city"){
+            setcity(e.target.value);
+        }
+    }
+    const weatherData = async (e)=>{
+        e.preventDefault();
+        if(city === ' '){
+            alert('fill city name first');
+        }
+        else{
+                var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherapi}`
+                const data = await fetch(url)
+                            .then((response) => response.json())
+                            .then( data=> data);
+
+                setweather(data);
+        }
+    }
+    // const  fetchApi = async (e)=>{
+    //     var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherapi}`
+    //     await fetch(url)
+    //     .then((response) => response.json())
+    //     .then( data=>{
+    //         console.log(data)
+    //         setcity(prevState => [...prevState,{
+    //             name:data.main.name
+    //         }])
+    //     })
+    //     .catch((err)=> console.log(err))
+    // }
+	return (
+	<>
+        <form>
+            <input 
+            type="text"
+             onChange={input}
+             name="city" 
+            />
+            <button onClick={weatherData} >Submit</button>
+        </form>
+        {city}
+	</>
+	);
 }
 
 export default App;
---
+++ src/api/weatherApi.js
@@ -0,0 +1,2 @@
+const api = '7a8aca1afefb7c238eaa4e45c8010f3d'
+export default api;
